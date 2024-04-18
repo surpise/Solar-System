b7545a32294ec3360f26dae55c9721532b8e40e5
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-04-18 19:32:08 +0900

    chore: path alias 설정 #1
    path alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@scripts': resolve(__dirname, './src/scripts'),
+      '@components': resolve(__dirname, './src/scripts/components'),
+      '@util': resolve(__dirname, './src/scripts/util'),
+      '@styles': resolve(__dirname, './src/styles'),
+    },
+  },
+});

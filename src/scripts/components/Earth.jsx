76fdfccabbf54ee0aae7c83a0a38a8621dd0e834
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-04-19 17:51:10 +0900

    feat: 지구 구현 #5
    지구 컴포넌트 구현
    공전 애니메이션 구현

---
+++ src/scripts/components/Earth.jsx
@@ -0,0 +1,11 @@
+import '@styles/components/Earth.css';
+
+const Earth = () => {
+  return (
+    <div className="earth-orbit">
+      <div className="earth"></div>
+    </div>
+  );
+};
+
+export default Earth;
--- src/scripts/components/Sun.jsx
+++ src/scripts/components/Sun.jsx
@@ -1,7 +1,13 @@
 import '@styles/components/Sun.css';
 
+import Earth from '@components/Earth';
+
 const Sun = () => {
-  return <div className="sun"></div>;
+  return (
+    <div className="sun">
+      <Earth />
+    </div>
+  );
 };
 
 export default Sun;

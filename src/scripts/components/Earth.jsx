94bc5fa0598e517711801fd2771f6d31e80619b2
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-04-20 19:19:47 +0900

    feat: 달 구현 #5
    달 구현
    달 애니메이션 구현

--- src/scripts/components/Earth.jsx
+++ src/scripts/components/Earth.jsx
@@ -3,7 +3,11 @@ import '@styles/components/Earth.css';
 const Earth = () => {
   return (
     <div className="earth-orbit">
-      <div className="earth"></div>
+      <div className="earth">
+        <div className="moon-orbit">
+          <div className="moon"></div>
+        </div>
+      </div>
     </div>
   );
 };

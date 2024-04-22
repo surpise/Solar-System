df5a4caf3e7cfa40ea1f050afbbb18f29aee84cb
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-04-22 18:09:36 +0900

    feat: 수성 구현 #7
    수성 구현
    자전 애니메이션을 따로 구현

---
+++ src/scripts/components/Mercury.jsx
@@ -0,0 +1,11 @@
+import '@styles/components/Mercury.css';
+
+const Mercury = () => {
+  return (
+    <div className="mercury-orbit">
+      <div className="mercury"></div>
+    </div>
+  );
+};
+
+export default Mercury;
--- src/scripts/components/Sun.jsx
+++ src/scripts/components/Sun.jsx
@@ -1,10 +1,12 @@
 import '@styles/components/Sun.css';
 
 import Earth from '@components/Earth';
+import Mercury from '@components/Mercury';
 
 const Sun = () => {
   return (
     <div className="sun">
+      <Mercury />
       <Earth />
     </div>
   );

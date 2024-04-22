65f11e3d672cfc336ed3a4652eabb23cbfde3095
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-04-22 18:17:14 +0900

    feat: 금성 구현 #7
    금성 구현

--- src/scripts/components/Sun.jsx
+++ src/scripts/components/Sun.jsx
@@ -2,11 +2,13 @@ import '@styles/components/Sun.css';
 
 import Earth from '@components/Earth';
 import Mercury from '@components/Mercury';
+import Venus from '@components/Venus';
 
 const Sun = () => {
   return (
     <div className="sun">
       <Mercury />
+      <Venus/>
       <Earth />
     </div>
   );
---
+++ src/scripts/components/Venus.jsx
@@ -0,0 +1,11 @@
+import '@styles/components/Venus.css';
+
+const Venus = () => {
+  return (
+    <div className="venus-orbit">
+      <div className="venus"></div>
+    </div>
+  );
+};
+
+export default Venus;

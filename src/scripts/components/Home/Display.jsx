76032a9242d45301c1dadf940d8e2987aa22aee9
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-05-02 09:43:41 +0900

    feat: display 드래그 기능 구현 #12
    드래그를 이용하여 화면 이동 기능 구현

--- src/scripts/components/Home/Display.jsx
+++ src/scripts/components/Home/Display.jsx
@@ -14,6 +14,8 @@ import Neptune from '@components/planetary-system/Neptune';
 
 const Display = () => {
   const [scale, setScale] = useState(1);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
 
   const handleZoomOut = () => {
     if (scale > 0.5) setScale(scale - 0.2);
@@ -23,9 +25,34 @@ const Display = () => {
     if (scale < 2) setScale(scale + 0.2);
   };
 
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setDragOffset({
+      x: e.clientX - e.currentTarget.offsetLeft,
+      y: e.clientY - e.currentTarget.offsetTop,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const newX = e.clientX - dragOffset.x;
+      const newY = e.clientY - dragOffset.y;
+      e.currentTarget.style.left = newX + 'px';
+      e.currentTarget.style.top = newY + 'px';
+    }
+  };
+
+  const handleMouseUp = () => setIsDragging(false);
+
   return (
     <>
-      <div className="display" style={{ transform: `scale(${scale})` }}>
+      <div
+        className="display"
+        style={{ transform: `scale(${scale})` }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
         <Sun />
         <Mercury />
         <Venus />
